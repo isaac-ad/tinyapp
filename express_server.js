06b3caba3e0202a8498f383ad6ebe98e1ef2436f
isaac-ad <isaacadnanu@gmail.com>
2023-02-20 23:19:37 -0500

    added additional endpoints

--- express_server.js
+++ express_server.js
@@ -1,13 +1,19 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 8080;
 const urlDatabase = {
-    "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
-app.get("/", (req, res)=>{
-    res.send("Hello");
-})
-app.listen(port, ()=>{
-console.log(`example app listening on port ${port}!`)
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.listen(port, () => {
+  console.log(`example app listening on port ${port}!`);
 });

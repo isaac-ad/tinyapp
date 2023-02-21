d11e6eae487281a18d974669798218584dbfe148
isaac-ad <isaacadnanu@gmail.com>
2023-02-21 02:00:27 -0500

    created a form that allowed a user to input a longURL

--- express_server.js
+++ express_server.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 8080;
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+function generateRandomString() {
+    const alphanumeric = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+    for (let i = 0; i < 6; i++) {
+      const randomIndex = Math.floor(Math.random() * alphanumeric.length);
+      randomString += alphanumeric[randomIndex];
+    }
+    return randomString;
+  }
+  
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello");
 });
@@ -18,11 +29,24 @@ app.get("/hello", (req, res) => {
 app.listen(port, () => {
   console.log(`example app listening on port ${port}!`);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
-    const templateVars = { urls: urlDatabase };
-    res.render("urls_index", templateVars);
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
   });
-  app.get("/urls/:id", (req, res) => {
-    const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-    res.render("urls_show", templateVars);
-  });
+
